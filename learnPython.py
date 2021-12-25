f6e93aa3293fc8ec391fe76924d3505716365ddb
b-kolaru <68828873+b-kolaru@users.noreply.github.com>
2021-12-25 17:47:28 -0500

    Create learnPython.py

---
+++ learnPython.py
@@ -0,0 +1,29 @@
+def days_to_unit(num_of_days, unit):
+    if unit == "hours":
+        return f"{num_of_days} days equals: {num_of_days * 24} {unit}"
+    elif unit == "minutes":
+        return f"{num_of_days} days equals: {num_of_days * 24 * 60} {unit}"
+    else:
+        return f"Please enter a proper unit of conversion(hours or minutes)"
+
+
+def validate_and_evaluate():
+    try:
+
+        user_input_num = int(user_element_dict["days"])
+        if user_input_num > 0:
+            calculate_hours = days_to_unit(user_input_num, user_element_dict["conversion_unit"])
+            print(calculate_hours)
+        elif user_input_num == 0:
+            print(f"Please enter a number other than Zero")
+        else:
+            print("Please enter a positive number")
+
+    except ValueError:
+        print("Please enter a valid number")
+
+
+user_input = input("Please enter the number of days and unit of conversion:\n")
+user_elements = user_input.split(":")
+user_element_dict = {"days": user_elements[0], "conversion_unit": user_elements[1]}
+validate_and_evaluate()
