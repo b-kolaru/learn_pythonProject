a8075792546eee5e48214d350eed290dd6f079ad
b-kolaru <68828873+b-kolaru@users.noreply.github.com>
2021-12-25 18:17:32 -0500

    Create loginAzure.py

---
+++ loginAzure.py
@@ -0,0 +1,12 @@
+from azure.identity import DefaultAzureCredential
+from azure.keyvault.keys import KeyClient
+
+url_path = "https://my-strong-vault.vault.azure.net/"
+
+credential = DefaultAzureCredential()
+
+key = KeyClient(vault_url=url_path, credential=credential)
+
+rsa_key = key.create_rsa_key("my-first-key", size=2048)
+print(rsa_key.name)
+print(rsa_key.key_type)
