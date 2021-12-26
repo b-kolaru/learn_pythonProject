85e344a62491eb9e3f044eafb8f3ea05209e9a4b
b-kolaru <68828873+b-kolaru@users.noreply.github.com>
2021-12-25 19:17:19 -0500

    removed f-string

--- learnPython.py
+++ learnPython.py
@@ -4,7 +4,7 @@ def days_to_unit(num_of_days, unit):
     elif unit == "minutes":
         return f"{num_of_days} days equals: {num_of_days * 24 * 60} {unit}"
 
-    
+
 def validate_and_evaluate():
     try:
 
@@ -13,7 +13,7 @@ def validate_and_evaluate():
             calculate_hours = days_to_unit(user_input_num, user_element_dict["conversion_unit"])
             print(calculate_hours)
         elif user_input_num == 0:
-            print(f"Please enter a number other than Zero")
+            print("Please enter a number other than Zero")
             
     except ValueError:
         print("Please enter a valid number")
